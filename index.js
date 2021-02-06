8c8f14f44bd94d609eb32967c59fcf17a9ddc062
Ben Penkacik <ben.penkacik@gmail.com>
2021-02-06 00:03:23 -0500

    get message sync working

--- index.js
+++ index.js
@@ -36,4 +36,58 @@ discordClient.on('message', message => {
     });
 });
 
+discordClient.on('message', message => {
+    if (message.content == 'dadbot channel id') {
+        message.channel.send(message.channel.id);
+    }
+});
 
+discordClient.on('message', message => {
+    if (message.content == 'dadbot guild id') {
+        message.channel.send(message.guild.id);
+    }
+});
+
+discordClient.on('message', message => {
+    console.log(message.content);
+    if (message.content.substring(0,4) == 'sync') {
+        let syncType = message.content.substring(5);
+        db.run('INSERT INTO guilds_channels_types(guild_id,channel_id,sync_name,guild_name,channel_name) VALUES(?,?,?,?,?)', [message.guild.id, message.channel.id, syncType, message.guild.name, message.channel.name], err => {
+            if (err) {
+                console.error(err);
+            }
+
+            message.channel.send('channel synced!');
+        })
+
+    }
+});
+
+discordClient.on('message', message => {
+    if (!message.author.bot) {
+        let query = 'SELECT sync_name, channel_id FROM guilds_channels_types WHERE channel_id = ?';
+        db.get(query, [message.channel.id], async (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+
+            if (result) {
+                query = 'SELECT guild_id, channel_id from guilds_channels_types where sync_name = ? AND channel_id != ?';
+                db.each(query, [result.sync_name, result.channel_id], (err, result) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                    console.log(result.channel_id);
+                    sendMessageToChannel(discordClient, result.channel_id, message);
+                });
+            }
+
+        });
+    }
+});
+
+async function sendMessageToChannel(client, channelId, message) {
+    let channel = await client.channels.fetch(channelId)
+    
+    channel.send(message.content);
+}
